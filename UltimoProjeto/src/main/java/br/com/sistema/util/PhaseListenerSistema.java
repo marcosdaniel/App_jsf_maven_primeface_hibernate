01e0837796376fbf4647f079d30dda92b95b7b9c
Marcos Daniel <marcosdaniel-br@hotmail.com>
2013-09-05 12:49:19 -0300

    FacesContextUtil

---
+++ UltimoProjeto/src/main/java/br/com/sistema/util/PhaseListenerSistema.java
@@ -0,0 +1,48 @@
+package br.com.sistema.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Marcos
+ */
+public class PhaseListenerSistema implements PhaseListener {
+    
+    //Antes da fase
+    @Override
+    public void beforePhase(PhaseEvent fase) {
+         if(fase.getPhaseId().equals(PhaseId.RESTORE_VIEW)){
+             System.out.println("Antes da fase: " +getPhaseId());
+             Session session = HibernateUtil.getSessionFactory().openSession();
+             session.beginTransaction();
+             FacesContextUtil.setRequestSession(session);
+         }
+    }
+    //Depois da fase
+    @Override
+    public void afterPhase(PhaseEvent fase) {
+        if(fase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)){
+            System.out.println("Depois da fase: " + getPhaseId());
+            Session session = FacesContextUtil.getRequestSession();
+            try {
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if(session.getTransaction().isActive()){
+                    session.getTransaction().rollback();
+                }
+            }finally{
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+    
+    
+}
